ebca5251f953f278133a157d66a9bff27e8ca78e
mapuchu <mapuchuz@gmail.com>
2015-03-19 11:05:20 +0100

    refactor read et list de article

---
+++ pages/poubelles.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+<!--
+<li id="<?php echo $article['id']; ?>">
+	<a href="index.php?page=article_read&id=<?php echo $article['id']; ?>"><?php echo $article['title']; ?></a>
+	- <a href="index.php?page=article_edit&id=<?php echo $article['id']; ?>">edit</a>
+	- <a href="index.php?page=article_delete&id=<?php echo $article['id']; ?>">delete</a>
+</li>
+-->
