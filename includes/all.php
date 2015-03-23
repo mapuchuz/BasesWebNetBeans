f36a11b4354f4f3748fdc2dc2e0a2ab15f1bb061
mapuchu <mapuchuz@gmail.com>
2015-03-23 11:58:05 +0100

    inclusion et instanciation dynamique du Repository

--- includes/all.php
+++ includes/all.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-//require("include/all.php");
 require("includes/db_connect.php");
 require("includes/functions.php");
 require("model/Article.php");
-require("model/ArticleRepository.php");
-require("pages/ArticleController.php");
 require("View/View.php");
--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <?php
 
-//print_r($_SERVER);
-
 /* initialisation */
 require("includes/all.php");
-
-$articlerepository= new ArticleRepository($db);
-
 $page = (isset($_GET['page']) ? $_GET['page'] : "article_list");
-
-/* analyse de la page demandée et création des variables */
-
-$montrerHtml = true;
-$html=  '';
-
 
 $control= strtok($page, "_");
 if($control=="article")
     $control=   "Article";
 $action= strtok("_");
 
+//inclusion dynamique du Repository
+require("model/" . $control . "Repository.php");
+//instantiation dynamique du Repository
+$repositoryName= $control . "Repository"; 
+$articlerepository= new $repositoryName($db);
+
+/* analyse de la page demandée et création des variables */
+$montrerHtml = true;
+$html=  '';
+
 if($control!="") {
-    $control.=      "Controller";
-    $controller= new $control($articlerepository);
+    $controlName=   $control . "Controller";
+    //inclusion dynamique
+    require("pages/" . $controlName . ".php");
+    //instantiation dynamique
+    $controller= new $controlName($articlerepository);
 
     if($action!="") {
         $action.= "Action";
@@ -32,44 +34,7 @@ if($control!="") {
         }
     }
 }
-/*
-switch ($page) {
-    case "register":
-        $titre = "Formulaire d'enregistrement";
-        $pageInclue = "pages/register.php";
-        break;
-    case "register_traitement":
-        $pageInclue = "pages/register_traitement.php";
-        $montrerHtml = false;
-        break;
-    case "article_read":
-        $titre = "Lecture d'un article";
-        $pageInclue=    'DEPRECATED';
-        $html=  $articleControl->readAction();
-        break;
-    case "article_list":
-        $titre = "Liste des articles";
-        $pageInclue=    'DEPRECATED';
-        $html=  $articleControl->listAction();
-        break;
-    case "article_add":
-    case "article_edit":
-        $titre = "Ajout/édition d'un article";
-        $pageInclue=    'DEPRECATED';
-        $html=  $articleControl->editAction();
-        break;
-    case "article_delete":
-        $titre = "Suppression d'un article";
-        $pageInclue=    'DEPRECATED';
-        $html=  $articleControl->deleteAction();
-        break;
-    case "home":
-    default:
-        $titre = "Page d'accueil";
-        $pageInclue = "pages/home.php";
-        break;
-}
-*/
+
 // si cette page a un affichage graphique, tout inclure, sinon juste un script
 if ($montrerHtml) {
     // le header contient le début de la page jusqu'à la balise <body>
